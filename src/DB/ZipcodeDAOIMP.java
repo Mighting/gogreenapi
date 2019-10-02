a79c28fedb530b49f88600fdc458cfacaadc6cc7
Mighting <Rasmusrosenkjaer@gmail.com>
2019-10-02 14:28:02 +0200

    Version 1.3.0.003 Added Zipcode in Models and Added ZipcodeDAOIMP

---
+++ src/DB/ZipcodeDAOIMP.java
@@ -0,0 +1,56 @@
+package DB;
+
+import models.Zipcode;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class ZipcodeDAOIMP implements StandardDAO<Zipcode> {
+
+
+    @Override
+    public Zipcode getFromID(int id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Zipcode> getAll() {
+        ArrayList<Zipcode> list = new ArrayList<Zipcode>();
+        try {
+            Connection con = MysqlCon.getCon();
+            Statement stmt = con.createStatement();
+            String qr = "SELECT * FROM zipcode";
+            ResultSet rs = stmt.executeQuery(qr);
+            while (rs.next()) {
+                String code = rs.getString("Zipcode");
+                String country = rs.getString("Country");
+                String city = rs.getString("City");
+                list.add(new Zipcode(code, country, city));
+            }
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    @Override
+    public int insert(Zipcode item) {
+        return  0;
+    }
+
+    @Override
+    public Zipcode add(Zipcode item) {
+        return null;
+    }
+
+    @Override
+    public Zipcode update(Zipcode item) {
+        return null;
+    }
+
+    @Override
+    public Zipcode disable(Zipcode item) {
+        return null;
+    }
+}
